0b2653ce87607c5fc5c527eb4b89986c9f4a95d9
Jefferson Garcia <jefferson.garciaor@gmail.com>
2023-06-05 16:09:54 -0500

    feat: agrega módulo que inicia la calculadora.

---
+++ run.py
@@ -0,0 +1,5 @@
+from calculadora.python.procesos_matematicos.procesos import sumar
+from  calculadora.python.front_calculadora.front_calculadora import *
+
+if __name__ == '__main__':
+    ventana_principal_calculadora()
