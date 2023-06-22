e8ca02da9b47d6d94ef641cf7fee874163d5b49d
Jefferson Garcia <jefferson.garciaor@gmail.com>
2023-06-22 13:33:26 -0500

    feat: agrega el módulo path para ubicar el logo de la calculadora.

---
+++ calculadora/python/metadata/path.py
@@ -0,0 +1,10 @@
+# Librerías externas.
+from pathlib import Path
+
+
+class PathImages:
+    """Rutas de imágenes usadas en la interfaz visual."""
+
+    img = Path.cwd() / 'img'
+    icono = img / 'calculator_icon.ico'
+    logo = img / 'logo_gob.png'
