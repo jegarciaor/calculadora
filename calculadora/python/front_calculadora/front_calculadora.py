d00dfd1491cecf5f88a6af26c409e825773485a3
Jefferson Garcia <jefferson.garciaor@gmail.com>
2023-06-22 21:42:15 -0500

    refactor: cambia los nombres de los métodos en la implementación de los botones..

--- calculadora/python/front_calculadora/front_calculadora.py
+++ calculadora/python/front_calculadora/front_calculadora.py
@@ -38,119 +38,119 @@ class VentanaCalculadora(customtkinter.CTk):
 
         self.boton_porcent = customtkinter.CTkButton(
             self.home_frame, text='%', font=self.font_botones,
-            command=lambda: self.agregar_calculo('%'), width=70, height=60)
+            command=lambda: self.ingresar_valores('%'), width=70, height=60)
         self.boton_porcent.place(x=0, y=65)
 
         # Creo los botones de la calculadora.
         boton_ce = customtkinter.CTkButton(
             self.home_frame, text='CE', font=self.font_botones,
-            command=lambda: self.agregar_calculo('CE'), width=70, height=60)
+            command=lambda: self.ingresar_valores('CE'), width=70, height=60)
         boton_ce.place(x=70, y=65)
 
         boton_c = customtkinter.CTkButton(
             self.home_frame, text='C', font=self.font_botones,
-            command=self.limpiar_campo, width=70, height=60)
+            command=self.borrar_campo, width=70, height=60)
         boton_c.place(x=140, y=65)
 
         boton_borrar = customtkinter.CTkButton(
-            self.home_frame, text='\u2190', font=self.font_botones,
-            command=self.limpiar_campo_parcial, width=70, height=60)
+            self.home_frame, text=chr(9003), font=self.font_botones,
+            command=self.borrar_campo_parcial, width=70, height=60)
         boton_borrar.place(x=210, y=65)
 
         boton_inverso = customtkinter.CTkButton(
             self.home_frame, text='1/x', font=self.font_botones,
-            command=self.convertir_inverso, width=70, height=60)
+            command=self.obtener_inverso, width=70, height=60)
         boton_inverso.place(x=0, y=126)
 
         boton_cuadrado = customtkinter.CTkButton(
             self.home_frame, text='x\u00B2', font=self.font_botones,
-            command=self.elevar_cuadrado, width=70, height=60)
+            command=self.obtener_cuadrado, width=70, height=60)
         boton_cuadrado.place(x=70, y=126)
 
         boton_raiz_cuadrada = customtkinter.CTkButton(
             self.home_frame, text='\u221A', font=self.font_botones,
-            command=self.raiz_cuadrada, width=70, height=60)
+            command=self.obtener_raiz_cuadrada, width=70, height=60)
         boton_raiz_cuadrada.place(x=140, y=126)
 
         boton_division = customtkinter.CTkButton(
             self.home_frame, text='\u00F7', font=self.font_botones,
-            command=lambda: self.agregar_calculo('/'), width=70, height=60)
+            command=lambda: self.ingresar_valores('/'), width=70, height=60)
         boton_division.place(x=210, y=126)
 
         boton_7 = customtkinter.CTkButton(
             self.home_frame, text='7', font=self.font_botones,
-            command=lambda: self.agregar_calculo(7), width=70, height=60)
+            command=lambda: self.ingresar_valores(7), width=70, height=60)
         boton_7.place(x=0, y=187)
 
         boton_8 = customtkinter.CTkButton(
             self.home_frame, text='8', font=self.font_botones,
-            command=lambda: self.agregar_calculo(8), width=70, height=60)
+            command=lambda: self.ingresar_valores(8), width=70, height=60)
         boton_8.place(x=70, y=187)
 
         boton_9 = customtkinter.CTkButton(
             self.home_frame, text='9', font=self.font_botones,
-            command=lambda: self.agregar_calculo(9), width=70, height=60)
+            command=lambda: self.ingresar_valores(9), width=70, height=60)
         boton_9.place(x=140, y=187)
 
         boton_multiplicacion = customtkinter.CTkButton(
             self.home_frame, text='x', font=self.font_botones,
-            command=lambda: self.agregar_calculo('*'), width=70, height=60)
+            command=lambda: self.ingresar_valores('*'), width=70, height=60)
         boton_multiplicacion.place(x=210, y=187)
 
         boton_4 = customtkinter.CTkButton(
             self.home_frame, text='4', font=self.font_botones,
-            command=lambda: self.agregar_calculo(4),
+            command=lambda: self.ingresar_valores(4),
             width=70, height=60)
         boton_4.place(x=0, y=248)
 
         boton_5 = customtkinter.CTkButton(
             self.home_frame, text='5', font=self.font_botones,
-            command=lambda: self.agregar_calculo(5), width=70, height=60)
+            command=lambda: self.ingresar_valores(5), width=70, height=60)
         boton_5.place(x=70, y=248)
 
         boton_6 = customtkinter.CTkButton(
             self.home_frame, text='6', font=self.font_botones,
-            command=lambda: self.agregar_calculo(6), width=70, height=60)
+            command=lambda: self.ingresar_valores(6), width=70, height=60)
         boton_6.place(x=140, y=248)
 
         boton_resta = customtkinter.CTkButton(
             self.home_frame, text='\u2212', font=self.font_botones,
-            command=lambda: self.agregar_calculo('-'), width=70, height=60)
+            command=lambda: self.ingresar_valores('-'), width=70, height=60)
         boton_resta.place(x=210, y=248)
 
         boton_1 = customtkinter.CTkButton(
             self.home_frame, text='1', font=self.font_botones,
-            command=lambda: self.agregar_calculo(1), width=70, height=60)
+            command=lambda: self.ingresar_valores(1), width=70, height=60)
         boton_1.place(x=0, y=309)
 
         boton_2 = customtkinter.CTkButton(
             self.home_frame, text='2', font=self.font_botones,
-            command=lambda: self.agregar_calculo(2), width=70, height=60)
+            command=lambda: self.ingresar_valores(2), width=70, height=60)
         boton_2.place(x=70, y=309)
 
         boton_3 = customtkinter.CTkButton(
             self.home_frame, text='3', font=self.font_botones,
-            command=lambda: self.agregar_calculo(3), width=70, height=60)
+            command=lambda: self.ingresar_valores(3), width=70, height=60)
         boton_3.place(x=140, y=309)
 
         boton_suma = customtkinter.CTkButton(
             self.home_frame, text='+', font=self.font_botones,
-            command=lambda: self.agregar_calculo('+'), width=70, height=60)
+            command=lambda: self.ingresar_valores('+'), width=70, height=60)
         boton_suma.place(x=210, y=309)
 
         boton_mas_menos = customtkinter.CTkButton(
             self.home_frame, text='\u00B1', font=self.font_botones,
-            command=self.cambiar_signo, width=70, height=60)
+            command=self.obtener_nuevo_signo, width=70, height=60)
         boton_mas_menos.place(x=0, y=371)
 
         boton_0 = customtkinter.CTkButton(
             self.home_frame, text='0', font=self.font_botones,
-            command=lambda: self.agregar_calculo(0), width=70, height=60)
+            command=lambda: self.ingresar_valores(0), width=70, height=60)
         boton_0.place(x=70, y=371)
 
         boton_cierra = customtkinter.CTkButton(
             self.home_frame, text='.', font=self.font_botones,
-            command=lambda: self.agregar_calculo('.'), width=70, height=60)
+            command=lambda: self.ingresar_valores('.'), width=70, height=60)
         boton_cierra.place(x=140, y=371)
 
         boton_igualdad = customtkinter.CTkButton(
